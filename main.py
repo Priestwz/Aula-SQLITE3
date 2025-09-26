70a66e5b8d5be6372ac94114c23acddeebba6b36
Henry <hudson.hm10282627@gmail.com>
2025-09-26 10:06:04 -0300

    add banco de dados

---
+++ main.py
@@ -0,0 +1,8 @@
+    #Criar um banco de daos SQLite e uma tabela
+import sqlite3
+
+#Criar a conexão com o banco de dados chamando de "escola.db"
+conexao = sqlite3.connect("escola.db")
+
+#Criar o objeto chamando de "cursor" que será usado para excutar os comandos sql
+cursor = conexao.cursor()
